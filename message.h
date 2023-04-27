85d300778f0cf100d748759f89548eef68881220
Blash20 <lash@royal-16.cs.wisc.edu>
2023-04-27 18:21:11 -0500

    updated message.h to to include both request and response

--- message.h
+++ message.h
@@ -2,14 +2,18 @@
 RCP does not define the format of messages. For this reason, we define a message struct here.
 */
 
-
+// message from client to server
 struct request_message{
     int seq_number;
     int client_id;
+    int type; // 1 for idle, 2 for get, 3 for put
+    int buf[2]; // other data like arguments
+};
 
-    //TODO: Figure out whether we need to have a number for init and close
-    int type; // 0 for init, 1 for idle, 2 for get, 3 for put, 4 for close
-
-    // other data like arguments
-    int buf[2];    
+// message from server to client
+struct response_message{
+    int seq_number;
+    int client_id;
+    int type; // 0 for ack, 1 for result
+    int result; // result of idle, get, or put
 };
